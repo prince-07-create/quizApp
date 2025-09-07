13c889eef4669495c836a45b9f31ef75e63fe641
prince-07-create <tkprince432@gmail.com>
2025-09-07 16:11:02 +0530

    Initial commit of the quiz app project

---
+++ src/main/java/com/project/quizApp/Model/Question.java
@@ -0,0 +1,26 @@
+package com.project.quizApp.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "quiz_questions")
+public class Question {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String questionTitle;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String rightAnswer;
+    private String difficultyLevel;
+    private String type;
+}
